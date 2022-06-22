10406482603ac13c700215d1f86814bd1e949cd8
Liddy01 <107370693+Liddy01@users.noreply.github.com>
2022-06-22 23:10:08 +0100

    Add files via upload

---
+++ zuri.js
@@ -0,0 +1,12 @@
+let firstName="Lydia";
+let lastName="Stephen";
+let country="Nigeria";
+let height="1.61m"
+console.log(firstName) 
+console.log(lastName)
+console.log(country)
+console.log(height)
+document.write("<h1> First Name:" + firstName)
+document.write("<h1> Last Name:" + lastName)
+document.write("<h1> Country:" + country)
+document.write("<h1> Height:" + height)
