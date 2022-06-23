8007c399e40ad7d15f0504b6c8bb5fddd7bccdd3
Liddy01 <107370693+Liddy01@users.noreply.github.com>
2022-06-24 00:10:35 +0100

    Update zuri.js

--- zuri.js
+++ zuri.js
@@ -1,12 +1,19 @@
-let firstName="Lydia";
-let lastName="Stephen";
-let country="Nigeria";
-let height="1.61m"
-console.log(firstName) 
-console.log(lastName)
-console.log(country)
-console.log(height)
-document.write("<h1> First Name:" + firstName)
-document.write("<h1> Last Name:" + lastName)
-document.write("<h1> Country:" + country)
-document.write("<h1> Height:" + height)
+//let firstName="Lydia";
+//let lastName="Stephen";
+//let country="Nigeria";
+//let height="1.61m"
+//console.log(firstName) 
+//console.log(lastName)
+//console.log(country)
+//console.log(height)
+//document.write("<h1> First Name:" + firstName)
+//document.write("<h1> Last Name:" + lastName)
+//document.write("<h1> Country:" + country)
+//document.write("<h1> Height:" + height)
+
+const name=prompt("Enter full name")
+const country=prompt(" Enter Country")
+const height=prompt(" Enter Height")
+
+alert("name: " + name + " " + "country: " + country + " " + " height: " + height);
+document.write("<h1>name:"+name+"country"+country+"height"+height)
